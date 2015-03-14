ad02621a8c8047c78c480f5f3df362162f9c1478
Paul C Pederson <ppederson@esri.com>
2015-03-13 17:26:30 -0700

    use tmp to build temporary folder

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var rimraf = require('rimraf')
 var shell = require('shelljs')
 var path = require('path')
 var glob = require('glob')
+var tmp = require('tmp')
 var fs = require('fs')
 
 module.exports = function (pathName, callback) {
@@ -9,14 +10,17 @@ module.exports = function (pathName, callback) {
   var pjson = require(path.resolve(process.cwd(), pathName, 'package.json'))
   var currentDir = path.basename(process.cwd())
   shell.cd(process.cwd())
-  shell.mkdir('-p', '../unignored-tmp')
-  shell.cd('../unignored-tmp')
+  var tmpDir = tmp.dirSync({
+    dir: '../',
+    unsafeCleanup: true
+  })
+  shell.cd(tmpDir.name)
   fs.writeFileSync('package.json', '{"name": "unignored-tmp","version": "0.0.0"}')
   shell.exec('npm install ../' + currentDir, {silent: true})
   var folderName = 'node_modules/' + pjson.name + '/'
   glob(folderName + '**', {nodir: true}, function (err, files) {
+    rimraf.sync(tmpDir.name)
     if (err) { callback(err) }
-    rimraf.sync('../unignored-tmp')
     files = files.map(function (file) {
       return file.replace(folderName, '')
     })
